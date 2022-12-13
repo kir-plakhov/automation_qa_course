034baa06182ee6ec4955a00ede5131ba1faae4d8
Kirill Plakhau <kirill.plakhau@outstaff.exness.com>
2022-12-13 16:47:51 +0300

    second_push

--- automation_qa_course/pages/elements_page.py
+++ automation_qa_course/pages/elements_page.py
@@ -1,4 +1,6 @@
 import time
+
+from generator.generator import generated_person
 from locators.elements_page_locators import TextBoxPageLocators
 from pages.base_page import BasePage
 
@@ -7,11 +9,19 @@ class TextBoxPage(BasePage):
     locators = TextBoxPageLocators()
 
     def fill_all_fields(self,):
-        self.element_is_visible(self.locators.FULL_NAME).send_keys('Kirill')
-        self.element_is_visible(self.locators.EMAIL).send_keys('newpartner1@yopmail.com')
-        self.element_is_visible(self.locators.CURRENT_ADDRESS).send_keys('Belarus, Minsk')
-        self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys('Belarus, Minsk')
+        person_info = next(generated_person())
+        full_name = person_info.full_name
+        email = person_info.email
+        current_address = person_info.current_address
+        permanent_address = person_info.permanent_address
+
+        self.element_is_visible(self.locators.FULL_NAME).send_keys(full_name)
+        self.element_is_visible(self.locators.EMAIL).send_keys(email)
+        self.element_is_visible(self.locators.CURRENT_ADDRESS).send_keys(current_address)
+        self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys(permanent_address)
         self.element_is_visible(self.locators.SUBMIT).click()
+
+        return full_name, email, current_address, permanent_address
 
     def check_filled_form(self):
         full_name = self.element_is_present(self.locators.CREATED_FULL_NAME).text.split(':')[1]
--- automation_qa_course/tests/elements_test.py
+++ automation_qa_course/tests/elements_test.py
@@ -8,9 +8,9 @@ class TestElements:
         def test_text_box(self, driver):
             text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
             text_box_page.open()
-            text_box_page.fill_all_fields()
-            output_name, output_email, output_cur_addr, output_per_addr = text_box_page.check_filled_form()
-            print(output_name)
-            print(output_email)
-            print(output_cur_addr)
-            print(output_per_addr)
+            input_data = text_box_page.fill_all_fields()
+            output_data = text_box_page.check_filled_form()
+
+            assert input_data == output_data
+
+
