31675528129e8d514a5d32f9a636aab8770fae3b
Kirill Plakhau <kirill.plakhau@outstaff.exness.com>
2022-12-21 16:23:19 +0300

    update elements tests added tests: - test_web_table_add_person - test_web_table_search_person

--- automation_qa_course/tests/elements_test.py
+++ automation_qa_course/tests/elements_test.py
@@ -1,5 +1,6 @@
+import random
 import time
-from pages.elements_page import TextBoxPage, CheckBoxPage, RadioButtonPage
+from pages.elements_page import TextBoxPage, CheckBoxPage, RadioButtonPage, WebTablePage
 
 
 class TestElements:
@@ -14,6 +15,7 @@ class TestElements:
             assert input_data == output_data
 
     class TestCheckBox:
+
         def test_check_box(self, driver):
             check_box_page = CheckBoxPage(driver, 'https://demoqa.com/checkbox')
             check_box_page.open()
@@ -25,6 +27,7 @@ class TestElements:
             assert input_checkbox == output_result
 
     class TestRadioButton:
+
         def test_radio_button(self, driver):
             radio_button_page = RadioButtonPage(driver, 'https://demoqa.com/radio-button')
             radio_button_page.open()
@@ -38,3 +41,19 @@ class TestElements:
             assert output_impressive == 'Impressive'
             assert output_no == 'No', '"No" has not been selected'
 
+    class TestWebTable:
+
+        def test_web_table_add_person(self, driver):
+            web_table_page = WebTablePage(driver, 'https://demoqa.com/webtables')
+            web_table_page.open()
+            new_person = web_table_page.add_new_person()
+            table_result = web_table_page.check_new_added_person()
+            assert new_person in table_result
+
+        def test_web_table_search_person(self, driver):
+            web_table_page = WebTablePage(driver, 'https://demoqa.com/webtables')
+            web_table_page.open()
+            key_word = web_table_page.add_new_person()[random.randint(0, 5)]
+            web_table_page.search_some_person(key_word)
+            table_result = web_table_page.check_search_person()
+            assert key_word in table_result, "person was not found in the table"
