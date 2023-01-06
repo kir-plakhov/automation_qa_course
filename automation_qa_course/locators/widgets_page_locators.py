cedad2b8a5a8ed456fc3d1a9b5a17e29b5d4f372
Kirill Plakhau <kirill.plakhau@outstaff.exness.com>
2023-01-06 15:31:04 +0300

    added tests for auto complete page

--- automation_qa_course/locators/widgets_page_locators.py
+++ automation_qa_course/locators/widgets_page_locators.py
@@ -8,3 +8,13 @@ class AccordianPageLocators:
     SECOND_SECTION_CONTENT = (By.CSS_SELECTOR, 'div[id="section2Content"] p')
     THIRD_SECTION = (By.CSS_SELECTOR, 'div[id="section3Heading"]')
     THIRD_SECTION_CONTENT = (By.CSS_SELECTOR, 'div[id="section3Content"] p')
+
+
+class AutoCompletePageLocators:
+    MULTI_INPUT = (By.CSS_SELECTOR, 'input[id="autoCompleteMultipleInput"]')
+    MULTI_VALUE = (By.CSS_SELECTOR, 'div[class="css-1rhbuit-multiValue auto-complete__multi-value"]')
+    MULTI_REMOVE_VALUE = (By.CSS_SELECTOR, 'div[class="css-1rhbuit-multiValue auto-complete__multi-value"] svg path')
+
+    SINGLE_VALUE = (By.CSS_SELECTOR, 'div[class="auto-complete__single-value css-1uccc91-singleValue"]')
+    SINGLE_INPUT = (By.CSS_SELECTOR, 'input[id="autoCompleteSingleInput"]')
+
--- automation_qa_course/tests/widgets_test.py
+++ automation_qa_course/tests/widgets_test.py
@@ -1,4 +1,6 @@
-from pages.widgets_page import AccordianPage
+import time
+
+from pages.widgets_page import AccordianPage, AutoCompletePage
 
 
 class TestWidgets:
@@ -13,3 +15,26 @@ class TestWidgets:
             assert first_title == 'What is Lorem Ipsum?' and first_content > 0
             assert second_title == 'Where does it come from?' and second_content > 0
             assert third_title == 'Why do we use it?' and third_content > 0
+
+    class TestAutoComplete:
+
+        def test_multi_auto_complete(self, driver):
+            auto_complete_page = AutoCompletePage(driver, 'https://demoqa.com/auto-complete')
+            auto_complete_page.open()
+            colors = auto_complete_page.fill_multi_input()
+            colors_result = auto_complete_page.check_color_in_multi()
+            assert colors == colors_result
+
+        def test_remove_value_from_multi(self, driver):
+            auto_complete_page = AutoCompletePage(driver, 'https://demoqa.com/auto-complete')
+            auto_complete_page.open()
+            auto_complete_page.fill_multi_input()
+            count_value_before, count_value_after = auto_complete_page.remove_value_from_multi_input()
+            assert count_value_before != count_value_after
+
+        def test_single_auto_complete(self, driver):
+            auto_complete_page = AutoCompletePage(driver, 'https://demoqa.com/auto-complete')
+            auto_complete_page.open()
+            input_color = auto_complete_page.fill_single_input()
+            result_color = auto_complete_page.check_color_in_single_input()
+            assert input_color == result_color
