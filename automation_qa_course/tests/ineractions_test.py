61c803b29786c7dab58e286d1fb0e33a81e2e80e
Kirill Plakhau <kirill.plakhau@outstaff.exness.com>
2023-01-10 13:42:41 +0300

    addedtests for sortable page

---
+++ automation_qa_course/tests/ineractions_test.py
@@ -0,0 +1,23 @@
+from pages.interactions_page import SortablePage
+
+
+class TestInteractions:
+    class TestSortablePage:
+
+        def test_sortable_tab(self, driver):
+            sortable_page = SortablePage(driver, 'https://demoqa.com/sortable')
+            sortable_page.open()
+            before, after = sortable_page.change_list_order()
+            assert before != after
+
+        def test_sortable_grid(self, driver):
+            sortable_page = SortablePage(driver, 'https://demoqa.com/sortable')
+            sortable_page.open()
+            before, after = sortable_page.change_grid_order()
+            assert before != after
+
+
+
+
+
+
